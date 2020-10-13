e33d228db087c6490e35fe391ac14a1e3965b0db
IlariaGalliano <ilaria.galliano7@gmail.com>
2020-10-13 17:13:45 +0200

    js-pwdgen-wannabe
    esercizio js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+* (insicurissimo) password generator
+*/
+
+// chiedi all'utente il nome
+var userName = prompt('Qual è il tuo nome?');
+
+// chiedi all'utente il cognome
+var lastName = prompt('Qual è il tuo cognome?')
+
+// chiedi all'utente il colore preferito
+var fav = prompt('Qual è il tuo colore preferito?')
+
+// Random password generator
+document.getElementById('password-generator').innerHTMl = userName + lastName + fav + "19";
